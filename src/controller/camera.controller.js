c1d62af3bf4c6f3a2c9a46dc36e24ab429fe32bb
emvalencaf <edsonmvf@gmail.com>
2022-09-20 23:23:33 -0300

    update Controller e View da Câmera

--- src/controller/camera.controller.js
+++ src/controller/camera.controller.js
@@ -3,21 +3,27 @@ export class CameraController{
     constructor(view){
 
         this.view = view
-    
+/*        
         navigator.mediaDevices.getUserMedia({
             video:true
         })
-            .then(stream => {
+            .then(mediaStream => {
 
-                this.view._el.videoCamera.src = URL.createObjectURL(stream)
-                this.view._el.videoCamera.play()
+
+                this.view._el.videoCamera.srcObject = mediaStream
+
+                this.view._el.videoCamera.onloadedmetadata = () =>{
+
+                    this.view._el.videoCamera.play()
+
+                }
 
             })
             .catch(err => {
 
                 console.error(err)
 
-            })
+            })*/
     }
 
 }
